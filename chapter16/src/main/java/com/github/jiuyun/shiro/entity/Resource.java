c8034da696131adb88bcfa75af1d7910cc531ede
1277809056@qq.com <123lijie->
2017-07-13 22:34:15 +0800

    哇啦啦啦啦+2

---
+++ chapter16/src/main/java/com/github/jiuyun/shiro/entity/Resource.java
@@ -0,0 +1,98 @@
+package com.github.jiuyun.shiro.entity;
+
+
+import java.io.Serializable;
+
+/**
+ * Created by Li Jie on 2017/7/13 0013.
+ */
+public class Resource implements Serializable{
+
+    private Long id;
+    private String name;
+    private ResourceType type = ResourceType.menu;//资源类型
+    private String url;
+    private String permission;
+    private Long parentId;
+    private Long parentIds;
+    private Boolean available = Boolean.FALSE;
+
+
+
+    public static enum ResourceType {
+        menu("菜单"), button("按钮");
+
+        private final String info;
+        private ResourceType(String info) {
+            this.info = info;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
+
+    public void setType(ResourceType type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public Long getParentIds() {
+        return parentIds;
+    }
+
+    public void setParentIds(Long parentIds) {
+        this.parentIds = parentIds;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
